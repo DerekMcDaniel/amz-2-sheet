753b641a1fd04f1d33d4b3232c564e33ce2625fd
Derek <30557517+DerekMcDaniel@users.noreply.github.com>
2018-03-16 14:37:14 -0400

    amz2sheets

---
+++ amz-2-sheets.py
@@ -0,0 +1,101 @@
+import bottlenose
+import xmltodict
+import json
+from pprint import pprint
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+from time import sleep
+
+
+scope = [
+    'https://spreadsheets.google.com/feeds',
+    'https://www.googleapis.com/auth/drive'
+]
+
+# Sign up for google sheet api to get credentials 
+creds = r""
+
+credentials = ServiceAccountCredentials.from_json_keyfile_name(creds, scope)
+
+gc = gspread.authorize(credentials)
+
+sh = gc.open('AMZ TEST')
+
+worksheet = sh.sheet1
+
+# Your amazon keys
+AMAZON_ACCESS_KEY = ""
+AMAZON_SECRET_KEY = ""
+AMAZON_ASSOC_TAG = ""
+
+amazon = bottlenose.Amazon(AMAZON_ACCESS_KEY, AMAZON_SECRET_KEY, AMAZON_ASSOC_TAG,
+                           )
+searches = ['Rubber Ducky']
+
+
+class AMZ:
+    row = 2
+
+    def get_results(self):
+        for searchterm in searches:
+            results = amazon.ItemSearch(
+                Keywords=searchterm,
+                SearchIndex="All",
+                ResponseGroup='Large,Reviews,OfferFull,Offers,OfferSummary,ItemAttributes,SalesRank')
+            sleep(1)
+            dictresults = dict(xmltodict.parse(results))
+            for i in dictresults['ItemSearchResponse']['Items']['Item']:
+                range_build = 'A' + str(self.row) + ':V' + str(self.row)
+                cell_list = worksheet.range(range_build)
+                cell_list[0].value = searchterm  # name
+                cell_list[1].value = i.get('ItemAttributes', {}).get('Manufacturer')
+                cell_list[2].value = i.get('ItemAttributes', {}).get('Title')  # name
+                cell_list[3].value = i.get('ASIN')  # asin
+                if i.get('BrowseNodes', {}).get('BrowseNode'):
+                    for x in i.get('BrowseNodes', {}).get('BrowseNode'):
+                        if not isinstance(x, str):
+                            cell_list[4].value = [x.get('Ancestors', {}).get('BrowseNode', {}).get('Name'),
+                                                  i.get('ItemAttributes', {}).get('Binding')]
+                cell_list[5].value = i.get('DetailPageURL')  # url
+                cell_list[6].value = i.get('OfferSummary', {}).get('TotalNew')  # Number of new for sale
+                cell_list[7].value = i.get('OfferSummary', {}).get('LowestNewPrice', {}).get('FormattedPrice')
+                # lowest price
+                if i.get('ItemAttributes', {}).get('UPCList', {}).get('UPCListElement'):
+                    cell_list[8].value = i['ItemAttributes']['UPCList']['UPCListElement']  # UPCs associated
+                else:
+                    cell_list[8].value = None
+                if i.get('SalesRank'):
+                    cell_list[9].value = i.get('SalesRank')  # sales rank, lower the better
+                cell_list[10].value = i.get('OfferSummary', {}).get('LowestUsedPrice', {}).get('FormattedPrice')
+                cell_list[11].value = i.get('OfferSummary', {}).get('TotalUsed')
+                cell_list[12].value = i.get('OfferSummary', {}).get('LowestRefurbishedPrice', {}).get('FormattedPrice')
+                cell_list[13].value = i['OfferSummary']['TotalRefurbished']
+                cell_list[14].value = float(i.get(
+                    'ItemAttributes', {}).get('PackageDimensions', {}).get('Length', {}).get(
+                    '#text', '0')) * .01
+                cell_list[15].value = float(i.get(
+                    'ItemAttributes', {}).get('PackageDimensions', {}).get('Width', {}).get(
+                    '#text', '0')) * .01
+                cell_list[16].value = float(i.get(
+                    'ItemAttributes', {}).get('PackageDimensions', {}).get('Height', {}).get(
+                    '#text', '0')) * .01
+                cell_list[17].value = float(i.get('ItemAttributes', {}).get(
+                    'PackageDimensions', {}).get('Weight', {}).get(
+                    '#text', '0')) * .01
+                if i.get('Offers', {}).get('Offer', {}).get('OfferListing', {}).get('IsEligibleForSuperSaverShipping')\
+                        == '1':
+                    cell_list[18].value = "Yes"
+                else:
+                    cell_list[18].value = "No"
+                if i.get('Offers', {}).get('Offer', {}).get('OfferListing', {}).get('IsEligibleForPrime') == '1':
+                    cell_list[19].value = "Yes"
+                else:
+                    cell_list[19].value = "No"
+                cell_list[20].value = i.get('Offers', {}).get('Offer', {}).get('Merchant', {}).get('Name')
+                cell_list[21].value = i.get('CustomerReviews', {}).get('HasReviews')
+                worksheet.update_cells(cell_list)
+                self.row += 1
+
+
+lets_go = AMZ()
+lets_go.get_results()
